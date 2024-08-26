00edd540109872c5007214ae3396c5ec9ca4824b
Tariq5mo <tariqomer756@gmail.com>
2024-08-26 17:06:45 +0300

    Fix count method

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,5 +29,5 @@ if __name__ == "__main__":
         HBNB_API_HOST = '0.0.0.0'
     HBNB_API_PORT = getenv("HBNB_API_PORT")
     if not HBNB_API_PORT:
-        HBNB_API_PORT = 5050
+        HBNB_API_PORT = 5000
     app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True, debug=True)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -111,6 +111,5 @@ class DBStorage:
         Args:
             cls (class, optional): The class object. Defaults to None.
         """
-        self.reload()
         all_cls_objs = self.all(cls)
         return len(all_cls_objs)
